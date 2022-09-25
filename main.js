ab8b65fb272d624b0f63a83a2336694ea8e0f505
MishellDallanaRoblesSerenoADSO <mishellrobles2004@gmail.com>
2022-09-25 17:37:48 -0500

    Ejercicio 2

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 addEventListener("DOMContentLoaded",()=>{
-    // #determinar si un numero es positivo y menor que 100
+    // Punto 2. calcular mayor de tres numeros
     
     // Datos
-    let num= Number(prompt("Ingrese el numero: ",68))
+    let num1= Number(prompt("Ingrese el primer valor: ",43))
+    let num2= Number(prompt("Ingrese el segundo valor: ",80))
+    let num3= Number(prompt("Ingrese el tercer valor: ",13))
     
-    if (num >0 && num <=100){
-        console.log(`El numero es positivo y menor a 100`)
+
+    if (num1>num2 && num1>num3){
+        console.log(`El numero 1 es mayor`)
     
-        }  else if (num <0) {
-            console.log(`El numero es negativo y menor a 100`)
-        }
+    }  else if (num2>num1 && num2>num3) {
+        console.log(`El numero 2 es mayor`)
+        
+    } else {
+        console.log(`El numero 3 es mayor`)
+    } 
 })
 
