01e4a779a0e38552254b02c966f17370ae64f519
MishellDallanaRoblesSerenoADSO <mishellrobles2004@gmail.com>
2022-09-26 23:39:07 -0500

    Ejercicio 9

--- main.js
+++ main.js
@@ -1,25 +1,17 @@
 addEventListener("DOMContentLoaded",()=>{
-    // 8. En cierta empresa se les paga a sus trabajadores de la siguiente forma: si el empleado es de planta,
-    //la hora trabajada se le paga a $20000, si el empleado es administrativo, la hora trabajada se le paga
-    //a $10000. Para calcular su pago es necesario conocer el total de horas trabajadas.
-        
-    // Datos
-    let trb= String(prompt("¿El trabajador es de (p)planta o (a)administrativo?: "))
+    //9. Realizar el algoritmo que lea N números, calcule y escriba la suma de los pares y el producto de los
+    //impares. Numero par es aquel que su residuo igual a cero (num mod !=0)
     
-    //Si el trabajador es de planta
-
-    if (trb=="p"){
-        ht1=Number(prompt("Horas trabajadas del empeado de planta: "))
-        h1=20000*ht1
-        console.log(`El pago por horas del trabajador de planta es: "${h1}"`)
-    }
-
-    //Si el valor es menor a 130000
-    if (trb=="a"){
-        ht2=Number(prompt("Horas trabajadas del empeado administrativo: "))
-        h2=10000*ht2
-        console.log(`El pago por horas del trabajador administrativo es: "${h2}"`)
+    let can=Number(prompt("Cuantos numeros desea ingresar?: "))
+    parseInt(can) 
+    
+    for (let a=1; a<=can; a++){
+        if (a %2 ==0 && a!=0){
+            console.log(`El numero,"${a}", es  par `)
+        }else{
+            console.log(`El numero "${a}", es impar`)
+        }
     }
     
-})
+    })
 
