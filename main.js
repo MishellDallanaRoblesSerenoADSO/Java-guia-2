12a906d266b2e1a80b986321157e37e5f821699f
MishellDallanaRoblesSerenoADSO <mishellrobles2004@gmail.com>
2022-09-27 01:27:57 -0500

    Ejercicio 11

--- main.js
+++ main.js
@@ -1,28 +1,19 @@
 addEventListener("DOMContentLoaded",()=>{
-    //10. Calcular el valor total del valor de 5 productos, el IVA y el subtotal, visualizar los resultados de:
-    //Iva, Subtotal y Total de la compra de los artículos.
+    //11. Programa para solicitar el nombre, apellido, edad y la nota promedio de 5 estudiantes de un curso
+    //de computación.
     
-    alert("Coloca el precio de 5 productos")
     
     //Datos
-    let p1=Number(prompt("Coloca el precio del producto 1: ",21))
-    let p2=Number(prompt("Coloca el precio del producto 2: ",64))
-    let p3=Number(prompt("Coloca el precio del producto 3: ",87))
-    let p4=Number(prompt("Coloca el precio del producto 4: ",89))
-    let p5=Number(prompt("Coloca el precio del producto 5: ",17))
+    let nom=prompt(" Coloque su nombre y apellido: ")
+    let edad=Number(prompt("Coloque su edad: ",17))
+    let n1=Number(prompt("Coloca la nota 1: ",8))
+    let n2=Number(prompt("Coloca la nota 2: ",6))
+    let ne=Number(prompt("Coloca la nota del examen: ",7))
+    let nf=Number(prompt("Coloca la nota final: ",7))
     
     //codigo
-
-    subt=(p1+p2+p3+p4+p5)
-
-
-    if (subt){
-        console.log(`El subtotal de la compra es "${subt}"`)
-
-        iva=subt*0.20
-        console.log(`El valor del iva incluido es de: "${iva}"`)
-        total=subt+iva
-        console.log(`Total a pagar es: "${total}"`) 
-    }
-    })
+    prom=(n1+n2+ne+nf)/4
+    console.log(`La nota del estudiante "${nom}", es de "${prom}"`)
+    
+})
 
