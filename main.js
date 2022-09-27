bf306e670cb96dce4af7057a2e4d637e74e4599e
MishellDallanaRoblesSerenoADSO <mishellrobles2004@gmail.com>
2022-09-27 02:02:41 -0500

    Ejercicio 13

--- main.js
+++ main.js
@@ -1,20 +1,40 @@
 addEventListener("DOMContentLoaded",()=>{
-    //12. Restarle al primer número al segundo (siempre y cuando el primero sea mayor que él segundo, en
-    //caso contrario indicar con un mensaje que la operación no es posible realizarla.
+    //13. En una tienda de HELADO da un descuento por compra a sus clientes con membresía dependiendo
+    //de su tipo, sólo existen tres tipos de membresía, tipo A, tipo B y tipo C.
+    //Los descuentos son los siguientes:
+    //Tipo A 10% de descuento
+    //Tipo B 15% de descuento
+    //Tipo C 20% de descuento
 
     
     
     //Datos
-    let num1=Number(prompt("Coloca el numero 1: ",18))
-    let num2=Number(prompt("Coloca el numero 2: ",11))
+    let helado=Number(prompt("Coloca el valor del helado: ",2500))
+    let mem=prompt("A que tipo de membresia pertenece A, B o C: ")
     
     //codigo
-    if (num1 > num2){
-        resta=num1 - num2
-        console.log(`El resultado de la resta es: "${resta}"`)
+    if (mem=="a"){
+        console.log(`Felicidades!, recibes un descuento del 10%`)
+        a= helado*0.10
+        des= helado-a
+        console.log(`El descuento fue de: "${a}"`)
+        console.log(`El valor total es: ""${des}`)
+    } 
+
+    if( mem=="b"){
+        console.log(`Felicidades!, recibes un descuento del 15%`)
+        b=helado*0.15
+        des1=helado-b
+        console.log(`El descuento fue de: "${b}"`)
+        console.log(`El valor total es: ""${des1}`)
+    }
     
-    }else
-    alert("No es posible realizar la resta")
-    
+    if (mem=="c"){
+        console.log(`Felicidades!, recibes un descuento del 20%`)
+        c=helado*0.20
+        des2=helado-c
+        console.log(`El descuento fue de: "${c}"`)
+        console.log(`El valor total es: ""${des2}`)
+    }
 })
 
