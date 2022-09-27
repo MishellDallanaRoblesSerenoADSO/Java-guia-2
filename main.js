599a8cc1268a6cbafdbdfc89f8b21e2cc73233f1
MishellDallanaRoblesSerenoADSO <mishellrobles2004@gmail.com>
2022-09-27 01:00:55 -0500

    Ejercicio 10

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
 addEventListener("DOMContentLoaded",()=>{
-    //9. Realizar el algoritmo que lea N números, calcule y escriba la suma de los pares y el producto de los
-    //impares. Numero par es aquel que su residuo igual a cero (num mod !=0)
+    //10. Calcular el valor total del valor de 5 productos, el IVA y el subtotal, visualizar los resultados de:
+    //Iva, Subtotal y Total de la compra de los artículos.
     
-    let can=Number(prompt("Cuantos numeros desea ingresar?: "))
-    parseInt(can) 
+    alert("Coloca el precio de 5 productos")
     
-    for (let a=1; a<=can; a++){
-        if (a %2 ==0 && a!=0){
-            console.log(`El numero,"${a}", es  par `)
-        }else{
-            console.log(`El numero "${a}", es impar`)
-        }
+    //Datos
+    let p1=Number(prompt("Coloca el precio del producto 1: ",21))
+    let p2=Number(prompt("Coloca el precio del producto 2: ",64))
+    let p3=Number(prompt("Coloca el precio del producto 3: ",87))
+    let p4=Number(prompt("Coloca el precio del producto 4: ",89))
+    let p5=Number(prompt("Coloca el precio del producto 5: ",17))
+    
+    //codigo
+
+    subt=(p1+p2+p3+p4+p5)
+
+
+    if (subt){
+        console.log(`El subtotal de la compra es "${subt}"`)
+
+        iva=subt*0.20
+        console.log(`El valor del iva incluido es de: "${iva}"`)
+        total=subt+iva
+        console.log(`Total a pagar es: "${total}"`) 
     }
-    
     })
 
